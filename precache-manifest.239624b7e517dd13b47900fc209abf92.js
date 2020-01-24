55e5b293acc8dc3d7017bf988d2014409b3665ca
Иван Лавренов <Viduck27@gmail.com>
2020-01-24 11:05:44 +0300

    Updates

---
+++ precache-manifest.239624b7e517dd13b47900fc209abf92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78f275c8b0e01d01117bdef4725dd24",
+    "url": "/WH_landing/index.html"
+  },
+  {
+    "revision": "c89fe3cd94ea00e6a5da",
+    "url": "/WH_landing/static/js/2.52227469.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/WH_landing/static/js/2.52227469.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36e5254abf5078829620",
+    "url": "/WH_landing/static/js/main.47fc31b5.chunk.js"
+  },
+  {
+    "revision": "2b336ee68852c694172c",
+    "url": "/WH_landing/static/js/runtime-main.4a593df5.js"
+  }
+]);
